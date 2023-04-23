a98738cb945ae6a5c3bfd13a8457fa5833114544
skytrobb <skytrobbins@gmail.com>
2023-04-23 02:03:12 -0600

    fix

--- src/components/NewerCard.tsx
+++ src/components/NewerCard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
@@ -9,6 +10,10 @@ import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Camera } from '../interfaces/rover.interface';
 import { ListItem, ListItemText, List } from '@mui/material';
 interface ExpandMoreProps extends IconButtonProps {
